8a6fa0f7bc52585b84a3e2201458b6419be11505
Changyuan Yang <26972260+hackycy@users.noreply.github.com>
2025-07-09 23:31:59 +0800

    chore: struct

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import uni from '@dcloudio/vite-plugin-uni'
-import UniHelperManifest from '@uni-helper/vite-plugin-uni-manifest'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [UniHelperManifest(), uni()],
+  plugins: [uni()],
 })
