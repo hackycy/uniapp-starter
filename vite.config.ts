29a86fe339045c7952f7f6a10f72f6e0ff162052
Leo Yang <qa894178522@qq.com>
2025-07-12 17:20:06 +0800

    chore: update vite cfg

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,21 @@ import UniPages from '@uni-helper/vite-plugin-uni-pages'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    UniManifest(),
-    UniPages({
-      dts: 'src/uni-pages.d.ts',
-    }),
-    Uni(),
-  ],
+export default defineConfig(() => {
+
+  const { UNI_PLATFORM } = process.env
+
+  return {
+    plugins: [
+      UniManifest(),
+      UniPages({
+        dts: 'src/uni-pages.d.ts',
+        subPackages: ['src/pages-sub'],
+      }),
+      Uni(),
+    ],
+    define: {
+      __UNI_PLATFORM__: UNI_PLATFORM
+    }
+  }
 })
