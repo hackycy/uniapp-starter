779c94fe9c39e9d0120b520e4e402abe08072e64
Leo Yang <qa894178522@qq.com>
2025-07-14 17:29:18 +0800

    chore: eslint config cjs rename to mjs

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
 import { onHide, onLaunch, onShow } from '@dcloudio/uni-app'
 
 onLaunch(() => {
-  // console.log('App Launch')
+  console.log('App Launch')
 })
+
 onShow(() => {
-  // console.log('App Show')
+  console.log('App Show')
 })
+
 onHide(() => {
-  // console.log('App Hide')
+  console.log('App Hide')
 })
 </script>
 
