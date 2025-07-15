c0556f557d52ac68d8d597662ab1a8362e2b2831
Leo Yang <qa894178522@qq.com>
2025-07-15 09:30:16 +0800

    feat: improve postinstall script

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { existsSync, writeFileSync } from 'node:fs'
+import { existsSync, rmSync, writeFileSync } from 'node:fs'
+import process from 'node:process'
+import UniManifest from '@uni-helper/vite-plugin-uni-manifest'
+import UniPages from '@uni-helper/vite-plugin-uni-pages'
+import { build } from 'vite'
 import { getRootPath } from './utils.mjs'
 
 /**
@@ -12,5 +16,49 @@ function ensureJsonFile(file) {
   }
 }
 
-ensureJsonFile('manifest')
-ensureJsonFile('pages')
+const IGNORE_EXTENSIONS = ['.vue', '.ts', '.tsx']
+
+/**
+ * 模拟启动一次构建生成 manifest.json 和 pages.json 文件
+ */
+await (async () => {
+  ensureJsonFile('manifest')
+  ensureJsonFile('pages')
+
+  await build({
+    configFile: false,
+    root: process.cwd(),
+    plugins: [
+      UniManifest(),
+      UniPages({
+        dts: 'src/uni-pages.d.ts',
+        subPackages: ['src/pages-sub'],
+      }),
+      // 拦截文件编译
+      [
+        {
+          name: 'mock-build-plugin',
+          enforce: 'pre',
+          load(id) {
+            const ext = id.split('.').pop()
+            if (IGNORE_EXTENSIONS.includes(`.${ext}`)) {
+              return '' // 返回空内容，跳过编译
+            }
+            return null
+          },
+        },
+      ],
+    ],
+  })
+
+  // 删除 dist 文件夹
+  const distPath = getRootPath('dist')
+  if (existsSync(distPath)) {
+    rmSync(distPath, {
+      recursive: true,
+      force: true,
+    })
+  }
+
+  console.log('Uni pages and manifest files generated successfully.')
+})()
