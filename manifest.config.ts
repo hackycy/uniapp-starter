aa7bc78a28266d0e57334c76796f45e9cfa40815
Leo Yang <qa894178522@qq.com>
2025-07-22 19:56:18 +0800

    chore: update manifest description

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,7 +1,7 @@
 import process from 'node:process'
 import { defineManifestConfig } from '@uni-helper/vite-plugin-uni-manifest'
 import { loadEnv } from 'vite'
-import { version } from './package.json'
+import { description, version } from './package.json'
 
 const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
@@ -10,7 +10,7 @@ const { UNI_WX_APPID, UNI_APP_PUBLIC_BASE } = env
 export default defineManifestConfig({
   'name': 'uni-helper',
   'appid': UNI_WX_APPID,
-  'description': '',
+  'description': description,
   'versionName': version,
   'versionCode': `${Number(version.replace(/\./g, ''))}`,
   'transformPx': false,
