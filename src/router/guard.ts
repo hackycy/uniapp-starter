7db2f48c671bb4d1899a872ee94493b8a7b5c505
Leo Yang <qa894178522@qq.com>
2025-08-07 10:27:19 +0800

    wip

--- src/router/guard.ts
+++ src/router/guard.ts
@@ -1,5 +1,13 @@
 import type { Router } from './types'
 
+const NAVIGATE_METHODS = ['navigateTo', 'redirectTo', 'switchTab']
+
 export function setupRouterGuard(_router: Router) {
-  // TODO
+  NAVIGATE_METHODS.forEach((method) => {
+    uni.addInterceptor(method, {
+      invoke(_args: UniApp.NavigateToOptions) {
+        return true
+      },
+    })
+  })
 }
