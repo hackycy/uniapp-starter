c16c79ff1b604a7d9c51bef81458d6e19af5a2b8
Changyuan Yang <26972260+hackycy@users.noreply.github.com>
2025-08-11 00:07:24 +0800

    wip

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { createSSRApp } from 'vue'
 import App from './App.vue'
 import { setupRouter } from './router'
 import { setupStore } from './store'
+import { setupDevtool } from './utils/env'
+
 import 'virtual:uno.css'
 
 export function createApp() {
   const app = createSSRApp(App)
   setupStore(app)
   setupRouter(app)
+  setupDevtool()
 
   return {
     app,
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,6 +3,7 @@ export function getAppEnvConfig() {
     VITE_APP_NAME: import.meta.env.VITE_APP_NAME,
     VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
     VITE_API_PATH: import.meta.env.VITE_API_PATH,
+    VITE_DEVTOOL: import.meta.env.VITE_DEVTOOL,
 
     // #ifdef H5
     VITE_OPENWX_APPID: import.meta.env.VITE_OPENWX_APPID,
@@ -12,4 +13,19 @@ export function getAppEnvConfig() {
     VITE_WX_APPID: import.meta.env.VITE_WX_APPID,
     // #endif
   }
+}
+
+let vconsole: unknown
+export async function setupDevtool() {
+  if (import.meta.env.VITE_DEVTOOL !== 'true') {
+    return
+  }
+
+  // #ifdef H5
+  if (vconsole) {
+    return
+  }
+  const { default: VConsole } = await import('vconsole')
+  vconsole = new VConsole()
+  // #endif
 }
