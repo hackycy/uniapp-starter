cfe9e2d38cafd88b8bdce30336678f9a5825aaa9
Leo Yang <qa894178522@qq.com>
2025-09-09 15:53:26 +0800

    wip

--- src/utils/http/alova.ts
+++ src/utils/http/alova.ts
@@ -2,12 +2,26 @@ import AdapterUniapp from '@alova/adapter-uniapp'
 import { createAlova } from 'alova'
 import VueHook from 'alova/vue'
 import { buildFullPath } from '..'
+import { isDev } from '../env'
 
 const alovaInstance = createAlova({
   baseURL: buildFullPath(import.meta.env.VITE_API_BASE_URL, import.meta.env.VITE_API_PATH),
   ...AdapterUniapp(),
   timeout: 10 * 1000,
   statesHook: VueHook,
+  /**
+   * https://alova.js.org/zh-CN/tutorial/cache/mode
+   * 缓存模式，默认情况下，GET 请求有 300000ms(5 分钟)的内存缓存时间
+   * 如果需要禁用缓存，可以在请求时设置 `cacheFor: null`
+   */
+  cacheFor: {
+    GET: 1 * 60 * 1000, // 1 分钟
+  },
+  /**
+   * https://alova.js.org/zh-CN/tutorial/advanced/in-depth/cache-logger
+   * 是否启用缓存日志打印，仅在开发环境下开启
+   */
+  cacheLogger: isDev(),
 })
 
 export const http = alovaInstance
