6a1cf750a54e66546847e5a713bb3944abbbad47
Leo Yang <qa894178522@qq.com>
2025-09-10 20:16:22 +0800

    feat: 增加无数据组件

---
+++ src/components/NoData/NoData.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+type StatusType = 'empty' | 'loading' | 'error'
+
+const props = defineProps({
+  status: {
+    type: String as PropType<StatusType>,
+    default: 'empty',
+  },
+  customClass: {
+    type: String,
+    default: '',
+  },
+  customStyle: {
+    type: String,
+    default: '',
+  },
+  emptyTip: {
+    type: String,
+    default: '暂无内容',
+  },
+  errorTip: {
+    type: String,
+    default: '加载失败',
+  },
+  extraTip: {
+    type: Array as PropType<[string | number, StatusType?][]>,
+    default: () => [],
+  },
+  icon: {
+    type: String,
+    default: '',
+  },
+})
+
+defineEmits(['click'])
+
+const getExtraTip = computed((): string[] => {
+  if (!props.extraTip) {
+    return []
+  }
+
+  return props.extraTip
+    .filter((item) => {
+      if (item.length === 1) {
+        return true
+      }
+      return item[1] === props.status
+    })
+    .map(item => `${item[0]}`)
+})
+</script>
+
+<script lang="ts">
+export default {
+  options: {
+    virtualHost: true,
+    addGlobalClass: true,
+    styleIsolation: 'shared',
+  },
+}
+</script>
+
+<template>
+  <view
+    class="box-border flex flex-col items-center justify-center gap-[32rpx]"
+    :class="customClass"
+    :style="customStyle"
+  >
+    <!-- 无数据/加载错误 -->
+    <view v-if="status === 'empty' || status === 'error'" class="box-border position-relative">
+      <slot :name="status">
+        <view class="flex flex-col items-center gap-[32rpx]" @click="$emit('click', status)">
+          <i
+            class="text-neutral-300 text-[180rpx] mt-[32rpx]"
+            :class="icon || status === 'empty' ? 'i-fluent-slide-search-16-regular' : 'i-uiw-reload'"
+          />
+
+          <!-- 标题 -->
+          <text class="text-[32rpx] font-bold text-gray-800">
+            {{ status === 'empty' ? emptyTip : errorTip }}
+          </text>
+
+          <!-- 额外提示 -->
+          <text v-if="getExtraTip.length > 0" class="text-[26rpx] text-gray-600 text-center leading-relaxed">
+            <view v-for="(tip, index) in getExtraTip" :key="index">
+              {{ tip }}
+            </view>
+          </text>
+        </view>
+      </slot>
+    </view>
+
+    <!-- 加载中 -->
+    <view v-if="status === 'loading'" class="box-border position-relative">
+      <slot name="loading">
+        <wd-loading />
+      </slot>
+    </view>
+
+    <!-- 追加内容 -->
+    <slot name="extra" :status="status" />
+  </view>
+</template>
