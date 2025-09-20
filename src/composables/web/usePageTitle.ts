c0b95e32fd724bd228d1719a1cb525abda8e90a0
Leo Yang <qa894178522@qq.com>
2025-09-20 10:54:56 +0800

    feat(usePageTitle): 修复恢复标题不生效

--- src/composables/web/usePageTitle.ts
+++ src/composables/web/usePageTitle.ts
@@ -1,12 +1,21 @@
 // #ifdef H5
 import { useTitle } from '@vueuse/core'
+import { getCurrentPage } from '@/router/helper'
 // #endif
 
+/**
+ * 设置页面标题，H5端用于返回页面时恢复之前的标题
+ * 其余端等同于调用 `uni.setNavigationBarTitle`
+ */
 export function usePageTitle() {
   // #ifdef H5
   const pageTitle = useTitle(null, {
-    restoreOnUnmount: (oldTitle) => {
-      return oldTitle
+    restoreOnUnmount: () => {
+      const returnPage = getCurrentPage() as Recordable | undefined
+      if (returnPage) {
+        return returnPage.$page?.meta?.navigationBar?.titleText || null
+      }
+      return null
     },
   })
   // #endif
@@ -14,6 +23,12 @@ export function usePageTitle() {
   function setTitle(title: string) {
     // #ifdef H5
     pageTitle.value = title
+    // #endif
+
+    // #ifndef H5
+    uni.setNavigationBarTitle({
+      title,
+    })
     // #endif
   }
 
