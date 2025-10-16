53c01239c49922dcf351013a29bc2c093ebf8750
Leo Yang <qa894178522@qq.com>
2025-10-16 16:34:11 +0800

    chore: update postinstall script

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -1,8 +1,19 @@
 // @ts-check
-import path from 'node:path'
-import process from 'node:process'
+import { copyFileSync, existsSync } from 'node:fs'
 import { checkManifestJsonFileSync } from '@uni-aide/vite-plugin-manifest'
 import { checkPagesJsonFileSync } from '@uni-aide/vite-plugin-pages'
+import { getRootPath } from './utils.mjs'
 
-checkPagesJsonFileSync(path.join(process.cwd(), 'src', 'pages.json'))
-checkManifestJsonFileSync(path.join(process.cwd(), 'src', 'manifest.json'))
+await (async () => {
+  // 检测 manifest.json 和 pages.json 文件
+  checkManifestJsonFileSync(getRootPath('src', 'manifest.json'))
+  checkPagesJsonFileSync(getRootPath('src', 'pages.json'))
+
+  // 创建开发环境env
+  const devEnvFile = getRootPath('.env.development')
+  if (!existsSync(devEnvFile)) {
+    copyFileSync(getRootPath('.env'), devEnvFile)
+  }
+
+  console.log('Development env create successfully.')
+})()
