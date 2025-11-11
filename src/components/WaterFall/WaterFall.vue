54936aeec5906a5cbe9439fac3d485fddb41f739
Changyuan Yang <26972260+hackycy@users.noreply.github.com>
2025-11-11 23:31:53 +0800

    wip(WaterFall)

---
+++ src/components/WaterFall/WaterFall.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({})
+</script>
+
+<script lang="ts">
+export default {
+  options: {
+    virtualHost: true,
+    addGlobalClass: true,
+    styleIsolation: 'shared',
+  },
+}
+</script>
+
+<template>
+  <view><!-- TODO --></view>
+</template>
+
+<style lang="scss" scoped></style>
